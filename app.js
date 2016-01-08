7d0656329c842511e4fff57d5f98db7ff284ee91
deepakpathania <deepakpathaniadp0@gmail.com>
2016-01-08 17:49:53 +0530

    live preview system added

--- app.js
+++ app.js
@@ -16,12 +16,22 @@ this.isSelected = function(checkTab) {
 };
 });
 
+app.controller('ReviewController', function() {
+this.review={}; 
+this.addReview=function(product) {
+	product.reviews.push(this.review);
+	this.review={};
+};
+
+});
+
 var gems= 
 [
 {
 	name: 'Gem',
 	price : 2.95,
 	description : 'good gem',
+	reviews :  [{stars: '5 stars' ,body:'must buy this,value for money.',author:'good@gmail.com'}],
 	canPurchase:true,
 	soldOut:false,
 	image:'cake.png',
@@ -30,6 +40,7 @@ var gems=
 	name: 'Gem2',
 	price : 3.95,
 	description : 'better gem',
+	reviews : [{stars : '1 star' , body : 'Bought this. I am broke now.', author : 'bad@gmail.com'}],
 	canPurchase:true,
 	soldOut:false,
 	image: 'circus.png',
